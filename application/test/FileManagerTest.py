aa9463aeb28d942b7433b343f87f769d254ce506
st084331 <71183199+st084331@users.noreply.github.com>
2023-05-27 22:38:04 +0300

    Not enough test check_dcm_in_folder

--- application/test/FileManagerTest.py
+++ application/test/FileManagerTest.py
@@ -6,58 +6,70 @@ from application.code.FileManager import FileManager
 from application.test.TestHelper import TestHelper
 
 types = ["median", "mode", "mean", "1", "3", "median_low", "median_high", "median_grouped"]
-CURRENT_PATH = os.getcwd()
-PARENT_PATH = os.path.abspath(os.path.join(CURRENT_PATH, os.pardir))
-CONFIG_PATH = os.path.join(PARENT_PATH, "test", "additional_files")
-DATA_PATH = os.path.join(PARENT_PATH, "test", "additional_files")
+CURRENT_FOLDER_PATH = os.getcwd()
+PARENT_FOLDER_PATH = os.path.abspath(os.path.join(CURRENT_FOLDER_PATH, os.pardir))
+CONFIG_FOLDER_PATH = os.path.join(PARENT_FOLDER_PATH, "test", "additional_files")
+DATA_FOLDER_PATH = os.path.join(PARENT_FOLDER_PATH, "test", "additional_files")
 
 
 class FileManagerTest(unittest.TestCase):
 
     def test_load_brightness_data(self):
-        TestHelper.make_csv_file(path=os.path.join(PARENT_PATH, DATA_PATH), file="test_brightness.csv",
+        paths = TestHelper.make_csv_file(path=DATA_FOLDER_PATH, file="test_brightness",
                                  data=[{"a": "a", "b": "b"}])
-        full_img_brightness_data = FileManager.load_brightness_data(data=DATA_PATH, file="test_brightness.csv")
+        full_img_brightness_data = FileManager.load_brightness_data(data_folder_path=DATA_FOLDER_PATH,
+                                                                    file_name="test_brightness.csv")
         self.assertEqual(full_img_brightness_data, [{"a": "a", "b": "b"}])
 
+        for path in paths:
+            os.remove(path)
+
     def test_load_data_for_fuzzy_criterion(self):
-        intersection = FileManager.load_data_for_fuzzy_criterion(config=CONFIG_PATH,
-                                                                 type_of_average="test")
+        paths1 = TestHelper.make_json_file(path=CONFIG_FOLDER_PATH, file='fuzzy_criterion_train_sick_in_intersection_test_load', data=[1])
+        paths2 = TestHelper.make_json_file(path=CONFIG_FOLDER_PATH, file='fuzzy_criterion_train_healthy_in_intersection_test_load', data=[1])
+
+        intersection = FileManager.load_data_for_fuzzy_criterion(config_folder_path=CONFIG_FOLDER_PATH,
+                                                                 type_of_average="test_load")
         self.assertEqual([[1], [1]], intersection)
 
+        for path in paths1:
+            os.remove(path)
+        for path in paths2:
+            os.remove(path)
+
     def test_load_data_for_most_powerful_criterion(self):
-        value = FileManager.load_data_for_most_powerful_criterion(config=CONFIG_PATH,
-                                                                  type_of_average="test")
+        paths = TestHelper.make_txt_file(path=CONFIG_FOLDER_PATH, file='most_powerful_criterion_train_test_load', data="1.0")
+
+        value = FileManager.load_data_for_most_powerful_criterion(config_folder_path=CONFIG_FOLDER_PATH,
+                                                                  type_of_average="test_load")
         self.assertEqual(value, 1.0)
+
+        for path in paths:
+            os.remove(path)
 
     def test_save_data_for_fuzzy_criterion(self):
         paths = FileManager.save_data_for_fuzzy_criterion(sick_in_intersection=[], healthy_in_intersection=[],
                                                           type_of_average="test_save",
-                                                          config=CONFIG_PATH)
-        self.assertTrue(os.path.exists(paths[0]))
-        self.assertTrue(os.path.exists(paths[1]))
+                                                          config_folder_path=CONFIG_FOLDER_PATH)
+        for path in paths:
+            self.assertTrue(os.path.exists(path))
+            os.remove(path)
 
-        if os.path.exists(paths[0]):
-            os.remove(paths[0])
-
-        if os.path.exists(paths[1]):
-            os.remove(paths[1])
 
     def test_save_data_for_most_powerful_criterion(self):
         path = FileManager.save_data_for_most_powerful_criterion(border_point=0, type_of_average="test_save",
-                                                                 config=CONFIG_PATH)
+                                                                 config_folder_path=CONFIG_FOLDER_PATH)
 
         self.assertTrue(os.path.exists(path))
-
-        if os.path.exists(path):
-            os.remove(path)
+        os.remove(path)
 
     def test_delete_residual_files(self):
+        TestHelper.make_txt_file(path=CONFIG_FOLDER_PATH, file="test_delete_residual_files", data="Delete it!")
 
-        FileManager.delete_residual_files(substr="test_delete_residual", path=os.path.join(PARENT_PATH, config))
+        FileManager.delete_residual_files(substr="test_delete_residual", folder_path=CONFIG_FOLDER_PATH)
 
         for i in range(3):
-            self.assertFalse(os.path.exists(os.path.join(PARENT_PATH, config, f"test_delete_residual_files_{i}.nii")))
+            self.assertFalse(os.path.exists(os.path.join(CONFIG_FOLDER_PATH, f"test_delete_residual_files_{i+1}.txt")))
 
 
 if __name__ == '__main__':
--- application/test/TestHelper.py
+++ application/test/TestHelper.py
@@ -1,8 +1,10 @@
 import csv
+import json
 import os
 
 
 class TestHelper:
+
     @staticmethod
     def make_csv_file(path, file, data, n_copy=1):
         if not os.path.exists(path):
@@ -10,11 +12,11 @@ class TestHelper:
         paths = []
         for i in range(n_copy):
             if n_copy > 1:
-                file_path = os.path.join(path, f"{file}_{i+1}")
+                file_path = os.path.join(path, f"{file}_{i+1}.csv")
             else:
-                file_path = os.path.join(path, f"{file}")
+                file_path = os.path.join(path, f"{file}.csv")
             with open(file_path, 'w') as csvfile:
-                writer = csv.DictWriter(csvfile, fieldnames=data.keys())
+                writer = csv.DictWriter(csvfile, fieldnames=data[0].keys())
                 writer.writeheader()
                 for dict in data:
                     writer.writerow(dict)
@@ -28,11 +30,26 @@ class TestHelper:
         paths = []
         for i in range(n_copy):
             if n_copy > 1:
-                file_path = os.path.join(path, f"{file}_{i + 1}")
+                file_path = os.path.join(path, f"{file}_{i + 1}.txt")
             else:
-                file_path = os.path.join(path, f"{file}")
-            f = open(file_path, "a")
-            f.write(data)
-            f.close()
+                file_path = os.path.join(path, f"{file}.txt")
+            with open(file_path, "a") as f:
+                f.write(data)
+                f.close()
+            paths.append(file_path)
+        return paths
+
+    @staticmethod
+    def make_json_file(path, file, data, n_copy=1):
+        if not os.path.exists(path):
+            os.mkdir(path)
+        paths = []
+        for i in range(n_copy):
+            if n_copy > 1:
+                file_path = os.path.join(path, f"{file}_{i + 1}.json")
+            else:
+                file_path = os.path.join(path, f"{file}.json")
+            with open(file_path, 'w') as f:
+                json.dump(data, f)
             paths.append(file_path)
         return paths
