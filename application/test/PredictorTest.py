02ca09fff0e20ffbb67ea7748c01018fee66e41b
st084331 <71183199+st084331@users.noreply.github.com>
2023-05-28 22:02:19 +0300

    added test_fuzzy_criterion_train_empty_sick_intersection, test_fuzzy_criterion_train_empty_healthy_intersection, test_fuzzy_criterion_train_empty_healthy_intersection

--- application/test/PredictorTest.py
+++ application/test/PredictorTest.py
@@ -19,7 +19,7 @@ class PredictorTest(unittest.TestCase):
                                                  whole_liver_brightness_data=whole_liver_brightness_data,
                                                  train_data=train_data)
 
-        self.assertEqual([[3.4], [3.5]], result)
+        self.assertEqual([[4.0, 3.4], [3.0, 3.5]], result)
 
     def test_fuzzy_criterion_train_empty_intersection(self):
         whole_liver_brightness_data = [{"nii": "a", "value": 1.0}, {"nii": "b", "value": 3.0}]
@@ -31,6 +31,28 @@ class PredictorTest(unittest.TestCase):
                                                  train_data=train_data)
 
         self.assertEqual([[], []], result)
+
+    def test_fuzzy_criterion_train_empty_sick_intersection(self):
+        whole_liver_brightness_data = [{"nii": "a", "value": 1.0}, {"nii": "b", "value": 3.0}]
+
+        train_data = [{"nii": "a", "ground_truth": 0.0}, {"nii": "b", "ground_truth": 0.0}]
+
+        result = Predictor.fuzzy_criterion_train(type_of_average="value",
+                                                 whole_liver_brightness_data=whole_liver_brightness_data,
+                                                 train_data=train_data)
+
+        self.assertEqual([[], [1.0, 3.0]], result)
+
+    def test_fuzzy_criterion_train_empty_healthy_intersection(self):
+        whole_liver_brightness_data = [{"nii": "a", "value": 1.0}, {"nii": "b", "value": 3.0}]
+
+        train_data = [{"nii": "a", "ground_truth": 1.0}, {"nii": "b", "ground_truth": 1.0}]
+
+        result = Predictor.fuzzy_criterion_train(type_of_average="value",
+                                                 whole_liver_brightness_data=whole_liver_brightness_data,
+                                                 train_data=train_data)
+
+        self.assertEqual([[1.0, 3.0], []], result)
 
     def test_fuzzy_criterion_train_empty_whole_liver_data(self):
         train_data = [{"nii": "a", "ground_truth": 1.0}, {"nii": "b", "ground_truth": 0.0}]
@@ -189,6 +211,10 @@ class PredictorTest(unittest.TestCase):
     def test_most_powerful_criterion_wrong_boarder_point(self):
         with self.assertRaises(ValueError):
             Predictor.most_powerful_criterion(value_of_brightness=1.0, boarder_point="b")
+
+    def test_fuzzy_criterion_train_empty_healthy_intersection(self):
+        self.assertEqual(0.25, Predictor.fuzzy_criterion(value_of_brightness=3.6, sick_intersection=[4.0, 3.4],
+                                        healthy_intersection=[3.0, 3.5]))
 
 
 if __name__ == '__main__':
