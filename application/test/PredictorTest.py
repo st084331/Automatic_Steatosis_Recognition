ce1033f034a7f33bce07802ad6d0c2122cf768dd
st084331 <71183199+st084331@users.noreply.github.com>
2023-05-28 16:52:28 +0300

    fuzzy_criterion_train tests for empty files

--- application/test/PredictorTest.py
+++ application/test/PredictorTest.py
@@ -50,6 +50,63 @@ class PredictorTest(unittest.TestCase):
         os.remove(os.path.join(CONFIG_FOLDER_PATH, 'fuzzy_criterion_train_sick_in_intersection_value.json'))
         os.remove(os.path.join(CONFIG_FOLDER_PATH, 'fuzzy_criterion_train_healthy_in_intersection_value.json'))
 
+    def test_fuzzy_criterion_train_empty_whole_liver_file(self):
+        file_path = os.path.join(CONFIG_FOLDER_PATH, "whole_liver.csv")
+        f = open(file_path, "a")
+        f.close()
+
+        paths = TestHelper.make_csv_file(path=CONFIG_FOLDER_PATH, file="train",
+                                         data=[{"nii": "a", "ground_truth": 1.0}, {"nii": "b", "ground_truth": 0.0}])
+
+        result = Predictor.fuzzy_criterion_train(type_of_average="value", data_folder_path=DATA_FOLDER_PATH,
+                                                 config_folder_path=CONFIG_FOLDER_PATH)
+
+        self.assertEqual([[], []], result)
+
+        for p in paths:
+            os.remove(p)
+        os.remove(file_path)
+        os.remove(os.path.join(CONFIG_FOLDER_PATH, 'fuzzy_criterion_train_sick_in_intersection_value.json'))
+        os.remove(os.path.join(CONFIG_FOLDER_PATH, 'fuzzy_criterion_train_healthy_in_intersection_value.json'))
+
+    def test_fuzzy_criterion_train_empty_train_file(self):
+        file_path = os.path.join(CONFIG_FOLDER_PATH, "train.csv")
+        f = open(file_path, "a")
+        f.close()
+
+        paths = TestHelper.make_csv_file(path=CONFIG_FOLDER_PATH, file="whole_liver",
+                                         data=[{"nii": "a", "value": 1.0}, {"nii": "b", "value": 0.0}])
+
+        result = Predictor.fuzzy_criterion_train(type_of_average="value", data_folder_path=DATA_FOLDER_PATH,
+                                                 config_folder_path=CONFIG_FOLDER_PATH)
+
+        self.assertEqual([[], []], result)
+
+        for p in paths:
+            os.remove(p)
+        os.remove(file_path)
+        os.remove(os.path.join(CONFIG_FOLDER_PATH, 'fuzzy_criterion_train_sick_in_intersection_value.json'))
+        os.remove(os.path.join(CONFIG_FOLDER_PATH, 'fuzzy_criterion_train_healthy_in_intersection_value.json'))
+
+    def test_fuzzy_criterion_train_empty_files(self):
+        file_path1 = os.path.join(CONFIG_FOLDER_PATH, "train.csv")
+        f = open(file_path1, "a")
+        f.close()
+
+        file_path2 = os.path.join(CONFIG_FOLDER_PATH, "whole_liver.csv")
+        f = open(file_path2, "a")
+        f.close()
+
+        result = Predictor.fuzzy_criterion_train(type_of_average="value", data_folder_path=DATA_FOLDER_PATH,
+                                                 config_folder_path=CONFIG_FOLDER_PATH)
+
+        self.assertEqual([[], []], result)
+
+        os.remove(file_path1)
+        os.remove(file_path2)
+        os.remove(os.path.join(CONFIG_FOLDER_PATH, 'fuzzy_criterion_train_sick_in_intersection_value.json'))
+        os.remove(os.path.join(CONFIG_FOLDER_PATH, 'fuzzy_criterion_train_healthy_in_intersection_value.json'))
+
 
 if __name__ == '__main__':
     unittest.main()
