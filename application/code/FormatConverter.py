c709182c5ce05944f93e2a38033fcd74a0f63a6e
st084331 <71183199+st084331@users.noreply.github.com>
2023-05-28 16:05:34 +0300

    Rollback sort()

--- application/code/FormatConverter.py
+++ application/code/FormatConverter.py
@@ -30,7 +30,4 @@ class FormatConverter:
             current_types.append(current_type)
 
         # print(f"End types_of_average_to_current_types: {current_types} |", datetime.now().strftime("%H:%M:%S.%f")[:-3])
-        if len(current_types) > 0:
-            return current_types.sort()
-        else:
-            return current_types
+        return current_types
