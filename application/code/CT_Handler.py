ad9d0e1f6fa3b91d69ff56e12f7ac4e45a4c4813
st084331 <71183199+st084331@users.noreply.github.com>
2023-05-30 03:46:03 +0300

    CT_Handler comments

--- application/code/CT_Handler.py
+++ application/code/CT_Handler.py
@@ -31,9 +31,12 @@ class CT_Handler:
         self.name_of_nifti: str = name_of_nifti_wo_extension + ".nii"
         self.mask_file_name: str = name_of_nifti_wo_extension + "-livermask2.nii"
 
+        # Convert Dicom folder to NifTi file
         self.nii_file_path: str = self.dicom_to_nifti(output_folder_path=dicom_to_nifti_output_folder_path)
 
-        self.mask_file_path: str = self.make_mask(output_folder_path=mask_output_folder_path, input_folder_path=dicom_to_nifti_output_folder_path,
+        # Getting a liver mask
+        self.mask_file_path: str = self.make_mask(output_folder_path=mask_output_folder_path,
+                                                  input_folder_path=dicom_to_nifti_output_folder_path,
                                                   cpu=cpu,
                                                   verbose=verbose,
                                                   vessels=vessels)
@@ -46,15 +49,18 @@ class CT_Handler:
         # print(f"dicom_to_nifti saved as {output} |", datetime.now().strftime("%H:%M:%S.%f")[:-3])
         return output_path
 
-    def make_mask(self, output_folder_path: str, input_folder_path: str, cpu: bool, verbose: bool, vessels: bool) -> str:
+    def make_mask(self, output_folder_path: str, input_folder_path: str, cpu: bool, verbose: bool,
+                  vessels: bool) -> str:
 
         # print("Start CT_Handler make_mask |", datetime.now().strftime("%H:%M:%S.%f")[:-3])
         output_folder_path_wo_extension: str = os.path.join(output_folder_path, self.name_of_nifti_wo_extension)
-        input: str = os.path.join(input_folder_path, self.name_of_nifti)
-        livermask.func(path=input, output=output_folder_path_wo_extension, cpu=cpu, verbose=verbose, vessels=vessels)
+        input_path: str = os.path.join(input_folder_path, self.name_of_nifti)
+        livermask.func(path=input_path, output=output_folder_path_wo_extension, cpu=cpu, verbose=verbose,
+                       vessels=vessels)
         # print(f"make_mask saved as {output_folder_path_wo_extension} |", datetime.now().strftime("%H:%M:%S.%f")[:-3])
         return os.path.join(output_folder_path, self.mask_file_name)
 
+    # Method for getting a list of hounsfield unit values of a NifTi file
     def get_whole_study_brightness_info(self) -> List[float]:
 
         whole_study = nib.load(self.nii_file_path)
@@ -63,6 +69,8 @@ class CT_Handler:
 
         whole_study_brightness = []
         # print("Start of collection whole_study_brightness |", datetime.now().strftime("%H:%M:%S.%f")[:-3])
+
+        # Step is 2 to speed up the collection process
         for z in range(0, whole_study_data.shape[0], 2):
             for y in range(0, whole_study_data.shape[1], 2):
                 for x in range(0, whole_study_data.shape[2], 2):
@@ -71,6 +79,7 @@ class CT_Handler:
 
         return whole_study_brightness
 
+    # Method for getting a list of hounsfield unit values of whole liver
     def get_whole_liver_brightness_info(self) -> List[float]:
 
         mask_img = nib.load(self.mask_file_path)
@@ -84,15 +93,18 @@ class CT_Handler:
         # print("Start of collection whole_liver_list_of_brightness |", datetime.now().strftime("%H:%M:%S.%f")[:-3])
         whole_liver_list_of_brightness = []
 
+        # Step is 2 to speed up the collection process
         for z in range(0, mask_data.shape[0], 2):
             for y in range(0, mask_data.shape[1], 2):
                 for x in range(0, mask_data.shape[2], 2):
+                    # If mask_data at this point is equal to 1, then this is the liver
                     if mask_data[z][y][x] == 1:
                         whole_liver_list_of_brightness.append(whole_study_data[z][y][x])
         # print("End of collection whole_liver_list_of_brightness |", datetime.now().strftime("%H:%M:%S.%f")[:-3])
 
         return whole_liver_list_of_brightness
 
+    # Method for getting a list of hounsfield unit values of three random areas of liver
     def get_three_areas_brightness_info(self) -> List[float]:
 
         mask_img = nib.load(self.mask_file_path)
@@ -113,11 +125,13 @@ class CT_Handler:
             rand_z = randint(0, mask_data.shape[0] - 1)
             rand_y = randint(0, mask_data.shape[1] - 1)
             rand_x = randint(0, mask_data.shape[2] - 1)
+            # Random again if the point is not from the liver
             while mask_data[rand_z][rand_y][rand_x] != 1:
                 rand_z = randint(0, mask_data.shape[0] - 1)
                 rand_y = randint(0, mask_data.shape[1] - 1)
                 rand_x = randint(0, mask_data.shape[2] - 1)
 
+            # Build an area around the received point and control the exit beyond the array boundary
             min_z = int(rand_z - diameter / 2)
             if min_z < 0:
                 min_z = 0
@@ -142,12 +156,14 @@ class CT_Handler:
             for z in range(min_z, max_z):
                 for y in range(min_y, max_y):
                     for x in range(min_x, max_x):
+                        # If mask_data at this point is equal to 1, then this is the liver
                         if mask_data[z][y][x] == 1:
                             three_areas_brightness.append(whole_study_data[z][y][x])
         # print("End of collection three_areas_brightness |", datetime.now().strftime("%H:%M:%S.%f")[:-3])
 
         return three_areas_brightness
 
+    # Method for getting a list of hounsfield unit values of two random areas of liver
     def get_two_areas_brightness_info(self) -> List[float]:
 
         mask_img = nib.load(self.mask_file_path)
@@ -197,12 +213,14 @@ class CT_Handler:
             for z in range(min_z, max_z):
                 for y in range(min_y, max_y):
                     for x in range(min_x, max_x):
+                        # If mask_data at this point is equal to 1, then this is the liver
                         if mask_data[z][y][x] == 1:
                             two_areas_brightness.append(whole_study_data[z][y][x])
         # print("End of collection two_areas_brightness |", datetime.now().strftime("%H:%M:%S.%f")[:-3])
 
         return two_areas_brightness
 
+    # Method for getting a list of hounsfield unit values of one random area of liver
     def get_one_area_brightness_info(self) -> List[float]:
 
         mask_img = nib.load(self.mask_file_path)
@@ -250,12 +268,14 @@ class CT_Handler:
         for z in range(min_z, max_z):
             for y in range(min_y, max_y):
                 for x in range(min_x, max_x):
+                    # If mask_data at this point is equal to 1, then this is the liver
                     if mask_data[z][y][x] == 1:
                         one_area_brightness.append(whole_study_data[z][y][x])
         # print("End of collection one_area_brightness |", datetime.now().strftime("%H:%M:%S.%f")[:-3])
 
         return one_area_brightness
 
+    # Method for getting a list of hounsfield unit values of 100 random points of liver
     def get_random_points_brightness_info(self) -> List[float]:
 
         mask_img = nib.load(self.mask_file_path)
@@ -273,6 +293,7 @@ class CT_Handler:
             rand_z = randint(0, mask_data.shape[0] - 1)
             rand_y = randint(0, mask_data.shape[1] - 1)
             rand_x = randint(0, mask_data.shape[2] - 1)
+            # Random again if the point is not from the liver
             while mask_data[rand_z][rand_y][rand_x] != 1:
                 rand_z = randint(0, mask_data.shape[0] - 1)
                 rand_y = randint(0, mask_data.shape[1] - 1)
