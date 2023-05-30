db5dafb429eca298fdfc59338a1137c91d524d45
st084331 <71183199+st084331@users.noreply.github.com>
2023-05-30 23:09:57 +0300

    main to init

--- application/code/Application.py
+++ application/code/Application.py
@@ -4,7 +4,6 @@ from datetime import datetime
 from PyQt5.QtWidgets import QApplication
 
 from application.code.FileManager import FileManager
-from application.code.FormatConverter import FormatConverter
 from application.code.Init import AVERAGES
 from application.code.MainWindow import MainWindow
 from typing import List
@@ -12,7 +11,7 @@ from typing import List
 
 class Application:
 
-    def main(self):
+    def __init__(self):
         # print("Start Application main()", datetime.now().strftime("%H:%M:%S.%f")[:-3])
 
         # Deleting files left after a critical program termination
@@ -24,13 +23,13 @@ class Application:
         # Creation of configuration files for fuzzy and most powerful criteria
         FileManager.make_config(averages=current_types)
 
-        app = QApplication(sys.argv)
+        self.app = QApplication(sys.argv)
 
-        window = MainWindow()
+        self.window = MainWindow()
 
-        window.show()
+        self.window.show()
 
-        app.exec()
+        self.app.exec()
 
         # print("End Application main()", datetime.now().strftime("%H:%M:%S.%f")[:-3])
 
@@ -38,5 +37,3 @@ class Application:
 if __name__ == "__main__":
     # print("Creating Application object |", datetime.now().strftime("%H:%M:%S.%f")[:-3])
     steatosis_recognizer = Application()
-
-    steatosis_recognizer.main()
